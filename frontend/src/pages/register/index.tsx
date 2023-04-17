50919fb25330d09c40cd82399383c650e8536019
tamerjar <algarmakany.ext@nubedian.de>
2023-04-17 15:23:28 +0300

    Removed unused vars

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -26,10 +26,6 @@ import InputAdornment from '@mui/material/InputAdornment'
 import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
 
 // ** Icons Imports
-import Google from 'mdi-material-ui/Google'
-import Github from 'mdi-material-ui/Github'
-import Twitter from 'mdi-material-ui/Twitter'
-import Facebook from 'mdi-material-ui/Facebook'
 import EyeOutline from 'mdi-material-ui/EyeOutline'
 import EyeOffOutline from 'mdi-material-ui/EyeOffOutline'
 import * as Sentry from "@sentry/nextjs"
