e1c57d752b501dc519e734c94180a5b4d063fef9
tamerjar <algarmakany.ext@nubedian.de>
2023-06-21 10:24:54 +0300

    Fixing deployment issues

--- miranda/app.js
+++ miranda/app.js
@@ -83,9 +83,5 @@ app.get('/search', async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
 
